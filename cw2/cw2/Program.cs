fd068e8a0fdff359531f76890d6f970a4d4b3828
makaron11 <makaron1711@gmail.com>
2024-03-12 15:20:27 +0100

    add avg

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -3,3 +3,17 @@
 Console.WriteLine("Hello World");
 Console.WriteLine("commit 2");
 Console.WriteLine("commit 3");
+
+static double Avg(int[] tab)
+{
+    int sum = 0;
+    foreach (int num in tab)
+    {
+        sum += sum + num;
+    }
+
+    double avg = sum / tab.Length;
+    return avg;
+} 
+
+Console.WriteLine(Avg(new []{1,2,3,4}));
