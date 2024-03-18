e711a77aa3b31583c604eb6a763f579cd757757b
makaron11 <makaron1711@gmail.com>
2024-03-18 20:28:29 +0100

    reset main to commit 3 zad2

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -4,16 +4,3 @@ Console.WriteLine("Hello World");
 Console.WriteLine("commit 2");
 Console.WriteLine("commit 3");
 
-static double Avg(int[] tab)
-{
-    int sum = 0;
-    foreach (int num in tab)
-    {
-        sum += num;
-    }
-
-    double avg = (double) sum / tab.Length;
-    return avg;
-} 
-
-Console.WriteLine(Avg(new []{1,2,3,4}));
